954eb14465720dd76f1a7a90d9bcde76115845a9
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-08-08 23:46:14 -0600

    Fixed a formating bug with the file I had here.

--- src/main/kotlin/leetcode/IntegerToRoman_13.kt
+++ src/main/kotlin/leetcode/IntegerToRoman_13.kt
@@ -39,115 +39,114 @@ package leetcode
  * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  *
  */
-class IntegerToRoman_13 {
 
-    /**
-     * Aug 6, 2023
-     *
-     *  input is a string representing a roman numeral.
-     *     return the roman numeral numerical value as an integer.
-     *
-     *     Observations:
-     *     written largest to smallest.
-     *     during exceptions, we subtract the smaller from the larger.
-     *
-     *     1. iterate through characters summing values.
-     *     2. keep track of most recently added roman numeral value.
-     *     3. if the new value is greater than the previous, subtract the previous from
-     *     the sum.
-     */
-    fun romanToInt1(s: String): Int {
-        var currentValue = 0
-        var previousValue = 1000;
-        var sum = 0
 
-        for (numeral in s) {
+/**
+ * Aug 6, 2023
+ *
+ *  input is a string representing a roman numeral.
+ *     return the roman numeral numerical value as an integer.
+ *
+ *     Observations:
+ *     written largest to smallest.
+ *     during exceptions, we subtract the smaller from the larger.
+ *
+ *     1. iterate through characters summing values.
+ *     2. keep track of most recently added roman numeral value.
+ *     3. if the new value is greater than the previous, subtract the previous from
+ *     the sum.
+ */
+fun integerToRoman_13_v1(s: String): Int {
+    var currentValue = 0
+    var previousValue = 1000;
+    var sum = 0
 
-            currentValue = when (numeral) {
-                'I' -> 1
-                'V' -> 5
-                'X' -> 10
-                'L' -> 50
-                'C' -> 100
-                'D' -> 500
-                else -> 1000
-            }
+    for (numeral in s) {
 
-            // for simplicity, I decided to add the integer value each time no matter what.
-            sum += currentValue
-
-            // we check to see if the previous value was smaller than the current value.
-            // if it's smaller, we were supposed to subtract it from the sum. This is why I subtract twice ( time 2 )
-            // once because I had added, and twice because I was supposed to subtract.
-            if (currentValue > previousValue) {
-                sum -= (previousValue * 2)
-            }
-
-            previousValue = currentValue
+        currentValue = when (numeral) {
+            'I' -> 1
+            'V' -> 5
+            'X' -> 10
+            'L' -> 50
+            'C' -> 100
+            'D' -> 500
+            else -> 1000
         }
 
-        return sum
-    }
+        // for simplicity, I decided to add the integer value each time no matter what.
+        sum += currentValue
 
-    /**
-     * I didn't realize before, but I had answered this question over a year ago, and it lets me see the growth I've made since then!"
-     */
-    fun romanToInt2(s: String): Int {
-
-        //iterate through romanToInt
-        //check for case of subtraction with nested if. add to count as needed.
-        //convert each numeral into the proper number.
-
-
-        var answer = 0
-
-        for (i in 0..s.lastIndex) {
-            if (s[i] == 'I') {
-                if (i != s.lastIndex) {
-                    if (s[i + 1] == 'V' || s[i + 1] == 'X') {
-                        answer += -1
-                        continue
-                    }
-                }
-
-                answer += 1
-            }
-
-            if (s[i] == 'V') answer += 5
-
-
-            if (s[i] == 'X') {
-                if (i != s.lastIndex) {
-                    if (s[i + 1] == 'L' || s[i + 1] == 'C') {
-                        answer += -10
-                        continue
-                    }
-
-                }
-                answer += 10
-            }
-
-            if (s[i] == 'L') answer += 50
-
-
-            if (s[i] == 'C') {
-                if (i != s.lastIndex) {
-                    if (s[i + 1] == 'D' || s[i + 1] == 'M') {
-                        answer += -100
-                        continue
-                    }
-
-                }
-                answer += 100
-            }
-
-            if (s[i] == 'D') answer += 500
-
-
-            if (s[i] == 'M') answer += 1000
-
+        // we check to see if the previous value was smaller than the current value.
+        // if it's smaller, we were supposed to subtract it from the sum. This is why I subtract twice ( time 2 )
+        // once because I had added, and twice because I was supposed to subtract.
+        if (currentValue > previousValue) {
+            sum -= (previousValue * 2)
         }
 
-        return answer
+        previousValue = currentValue
     }
+
+    return sum
+}
+
+/**
+ * I didn't realize before, but I had answered this question over a year ago, and it lets me see the growth I've made since then!"
+ */
+fun integerToRoman_13_v2(s: String): Int {
+
+    //iterate through romanToInt
+    //check for case of subtraction with nested if. add to count as needed.
+    //convert each numeral into the proper number.
+
+
+    var answer = 0
+
+    for (i in 0..s.lastIndex) {
+        if (s[i] == 'I') {
+            if (i != s.lastIndex) {
+                if (s[i + 1] == 'V' || s[i + 1] == 'X') {
+                    answer += -1
+                    continue
+                }
+            }
+
+            answer += 1
+        }
+
+        if (s[i] == 'V') answer += 5
+
+
+        if (s[i] == 'X') {
+            if (i != s.lastIndex) {
+                if (s[i + 1] == 'L' || s[i + 1] == 'C') {
+                    answer += -10
+                    continue
+                }
+
+            }
+            answer += 10
+        }
+
+        if (s[i] == 'L') answer += 50
+
+
+        if (s[i] == 'C') {
+            if (i != s.lastIndex) {
+                if (s[i + 1] == 'D' || s[i + 1] == 'M') {
+                    answer += -100
+                    continue
+                }
+
+            }
+            answer += 100
+        }
+
+        if (s[i] == 'D') answer += 500
+
+
+        if (s[i] == 'M') answer += 1000
+
+    }
+
+    return answer
 }
