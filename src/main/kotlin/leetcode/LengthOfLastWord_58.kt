6fa46f58edea4a85937a7cd480d3d3bfa5674a1d
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-08-17 07:41:36 -0600

    Completed Leetcode #58 Length of Last Word.

--- src/main/kotlin/leetcode/LengthOfLastWord_58.kt
+++ src/main/kotlin/leetcode/LengthOfLastWord_58.kt
@@ -4,8 +4,7 @@ package leetcode
  * Given a string s consisting of words and spaces, return the length of the last word in the string.
  *
  * A word is a maximal
- * substring
- *  consisting of non-space characters only.
+ * substring*  consisting of non-space characters only.
  *
  *
  *
